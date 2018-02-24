bec2bf0d711abc27c2caf18c7eeea176081dbfa0
Mattrg <Mattrg@users.noreply.github.com>
2018-02-24 13:08:54 -0800

    added player class

--- matthewG.cpp
+++ matthewG.cpp
@@ -1,1 +1,42 @@
+//Author: Matthew Gonzales
 //
+//
+#include "header.h"
+
+Image play[1] = {"fighterZLeft.png"};
+
+class player {
+	public:
+		int health;
+		int xres, yres;
+		int playerFrame;
+		Vec box[20];
+		player() {
+			health = 100;
+			xres = 609;
+			yres = 266;
+			playerFrame = 0;
+		}
+
+} pl;
+
+void playerHealthRender() {
+	Rect r;
+	r.bot = 675;
+	r.left = 10;
+	r.center = 0;
+	ggprint8b(&r, 16, 0x00ffff00, "Health: %i", pl.health);
+
+}
+
+void playerRender() {
+
+}
+
+void takePunch() {
+	health -= 5;
+}
+
+void takeKick() {
+	health -= 10;
+}
