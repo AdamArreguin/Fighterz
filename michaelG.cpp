54f7c24b78f3c61aaee5dd4c38e9ddbada649f81
miguz1329 <35708461+miguz1329@users.noreply.github.com>
2018-02-24 16:49:16 -0800

    Add files via upload

--- michaelG.cpp
+++ michaelG.cpp
@@ -1,1 +1,28 @@
 //
+#include "header.h"
+
+void displayScore(const char* s, int xres, int i)
+{
+
+	Rect r;
+	r.bot = xres -50;
+
+switch(i){
+	case 1:
+		r.left =400;
+		break;
+	case 2:
+		r.left =800;
+		break;
+	}
+
+	ggprint16(&r,32, 0x00ffffff, s);
+}		
+
+
+
+
+
+
+
+
